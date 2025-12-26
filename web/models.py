6591f23ac76d8b242a7031d7196f1dff65286b7d
Pouya <pouya.zm83@gmail.com>
2025-12-26 15:16:42 +0330

    models fix

--- web/models.py
+++ web/models.py
@@ -20,3 +20,7 @@ class Income(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.CharField(max_length=255)
     date = models.DateField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"{self.user.username} - {self.amount} on {self.date}"
